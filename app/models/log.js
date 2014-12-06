ebfb43748a02d4a66a918d60c37cefb2f32c98ca
comentor <loghire@gmail.com>
2014-12-06 16:13:19 +0200

    Support firebase for logs

--- app/models/log.js
+++ app/models/log.js
@@ -4,17 +4,3 @@ export default DS.Model.extend({
 	time: DS.attr('string'),
 	msg: DS.attr('string')
 });
-
-App.ApplicationRoute = Ember.Route.extend({
-	model: function() {
-		this.store.push('album', {
-			id: 1,
-			time: "Game started eventually"
-		});
-
-		this.store.push('album', {
-			id: 2,
-			time: "Game ended"
-		});
-	}
-});
