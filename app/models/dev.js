a163d6340e2135d50ed2761da4e731b53f41192a
Dmitry Banasko <dimon.bad@gmail.com>
2014-12-06 22:05:34 +0200

    added: dev skills

--- app/models/dev.js
+++ app/models/dev.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	name: DS.attr('string'),
-	salary: DS.attr('number')
+	salary: DS.attr('number'),
+	skills: DS.hasMany('skill', { async: true })
 });
