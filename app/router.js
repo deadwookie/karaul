7c0248a89395fa26708ab114cd8d021394fa2104
Pavel Zet <zetorama@gmail.com>
2014-12-06 20:30:18 +0200

    reorganize game routes

--- app/router.js
+++ app/router.js
@@ -2,15 +2,24 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+	location: config.locationType
 });
 
 Router.map(function() {
-	this.resource('game', function(){
-		this.route('email');
-		this.route('log');
+	this.resource('play', function() {
+		this.resource('game', {path: '/:game'}, function() {
+			this.resource('scrum');
+
+			this.route('project');
+			this.route('jobmarket');
+			this.route('email');
+			this.route('log');
+		});
 	});
+
 	this.route('rules');
+
+
 	this.route('top');
 	this.route('task');
 });
