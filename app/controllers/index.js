6a1e46b3217e852ebe26982e8b8c294a682903dd
Dmitry Banasko <dimon.bad@gmail.com>
2014-12-06 22:49:22 +0200

    added: generate game model onPlay

---
+++ app/controllers/index.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+	actions: {
+		play: function() {
+			var record = this.store.createRecord('game');
+
+			// @TODO: get basic game data from config
+			// @TODO: generate:
+			// -- devpool
+			// -- project
+			// etc..
+			record.setProperties({
+				sprintCount: 40,
+				yearCount: 4
+			});
+
+			return record.save().then(function(game) {
+				return this.transitionToRoute('/play/' + game.get('id'));
+			}.bind(this));
+		}
+	}
+});
