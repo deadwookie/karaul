c6a73042122b7f88836922970b66c0401c6c8a51
comentor <loghire@gmail.com>
2014-12-06 15:23:23 +0200

    some dev

--- app/models/log.js
+++ app/models/log.js
@@ -1,5 +1,20 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+	time: DS.attr('string'),
+	msg: DS.attr('string')
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+	model: function() {
+		this.store.push('album', {
+			id: 1,
+			time: "Game started eventually"
+		});
+
+		this.store.push('album', {
+			id: 2,
+			time: "Game ended"
+		});
+	}
 });
