bed5a7926c6f3d864a5ff37380f4dd866bf62797
makanaki <makanaki@gmail.com>
2014-12-07 18:38:16 +0200

    sprint

--- app/models/sprint.js
+++ app/models/sprint.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	name: DS.attr('string'),
-	pool: DS.hasMany('task', { async: true })
+	tasks: DS.hasMany('task', { async: true }),
+	devs: DS.hasMany('dev', { async: true }),
+	setup: DS.attr('setups')
 });
