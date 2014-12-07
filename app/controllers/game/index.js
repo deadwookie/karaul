1962254e041f16c2b816edf23e43726dff10b681
Dmitry Banasko <dimon.bad@gmail.com>
2014-12-07 13:07:45 +0200

    fixed: isPlayer computed property

--- app/controllers/game/index.js
+++ app/controllers/game/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	isPlayer: function() {
-		return this.get('model.players').contains(this.get('auth.user'));
-	}
+		return this.get('model.creator') === this.get('auth.user') || this.get('model.players').contains(this.get('auth.user'));
+	}.property('creator', 'players')
 });
